d14bb80735ec8c0f31b569613e25b79a64bf1cb9
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-07-12 19:21:52 +0800

    export blob db

---
+++ include/rocksdb/utilities/blob_db.h
@@ -0,0 +1,14 @@
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is also licensed under the GPLv2 license found in the
+//  COPYING file in the root directory of this source tree.
+//
+
+#ifndef ROCKSDB_LITE
+#pragma once
+
+#include "utilities/blob_db/blob_db.h"
+
+#endif  // ROCKSDB_LITE
